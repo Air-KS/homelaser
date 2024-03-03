023f93aa88aaae3f0fbb19bc5d0ea230d8802a7f
Air-KS <kevinrogeret@gmail.com>
2024-03-04 00:13:46 +0100

    [Create] - New function : Switch_Coolor, Start_Effect, Led_Intensity, sound, Reflex_Time, endGameEffect

---
+++ Application/src/main.ino
@@ -0,0 +1,390 @@
+#include "main.h"
+#include "prototypes.h"
+#include "broches.h"
+
+// Définit les paramètres pour les LED : luminosité, couleurs et palette
+uint8_t brightness;
+CRGB color_leds[NUM_LEDS];
+CRGBPalette16 palette = RainbowColors_p;
+
+
+// Configuration des signaux infrarouges
+const uint16_t kRecvPin = 36; // Broche de réception
+IRrecv irrecv(kRecvPin); // Initialisation du récepteur infrarouge
+decode_results results; // Stockage des résultats des signaux
+const uint32_t kBaudRate = 115200; // Taux de bauds pour la communication série
+
+
+// États des différents modes et fonctionnalités
+bool switch_colorLed = false;
+bool loading_active = false;
+bool buzzer_ON = true;
+
+
+/*
+*************************************************
+----- Variable pour les modes de jeux -----
+*************************************************
+*/
+// Réflex Times
+bool reflex_time_active = false;
+bool reflexTimeRunning = false;
+
+
+/*
+    --------- --------- --------- --------- ---------- ---------- ----------
+  --------- --------- --------- ------------- ---------- ---------- ----------
+--------- --------- --------- ----------------- ---------- ---------- ----------
+  --------- --------- --------- ------------- ---------- ---------- ----------
+    --------- --------- --------- --------- ---------- ---------- ----------
+*/
+
+
+/*
+*************************************************
+----- Initialisation des paramètres -----
+*************************************************
+*/
+void setup()
+{
+	// Initialise la communication et Attend qu'elle soit prête = 115200
+	Serial.begin(kBaudRate);
+	while (!Serial)
+	{
+		delay(50);
+	}
+
+	// Initialise les LED's, définit la luminosité et active la réception infrarouge
+	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(color_leds, NUM_LEDS);
+	FastLED.setBrightness(30);
+	irrecv.enableIRIn();
+
+	// Configuration des broches pour les différentes fonctionnalités
+	pinMode(REFLEX_TIME, INPUT_PULLUP);
+	pinMode(SWITCH_COLOR, INPUT_PULLUP);
+	pinMode(SOUND_PIN, INPUT_PULLUP);
+	pinMode(tonePin, OUTPUT);
+}
+
+
+/*
+*************************************************
+----- Boucle principale du programme -----
+*************************************************
+*/
+void loop()
+{
+	// Vérifiez si le mode d'entraînement AIM doit être activé
+	if (digitalRead(REFLEX_TIME) == HIGH && !reflex_time_active)
+	{
+		reflex_time_active = true;
+		switch_colorLed = false;
+		loading_active = true;
+		Reflex_Time();
+	}
+	else if (digitalRead(REFLEX_TIME) == LOW && reflex_time_active)
+	{
+		reflex_time_active = false;
+		loading_active = false;
+		fill_solid(color_leds, NUM_LEDS, CRGB::Black); // Éteindre toutes les LEDs
+		FastLED.show();
+	}
+	else if (digitalRead(SWITCH_COLOR) == LOW)
+	{
+		reflex_time_active = false;
+		loading_active = false;
+		if (!switch_colorLed)
+		{
+			loading_active = false;
+			fill_solid(color_leds, NUM_LEDS, CRGB::White);
+			FastLED.show();
+		}
+		Switch_Color();
+	}
+	else if (!reflex_time_active && loading_active)
+	{
+		loading_active = false;
+		fill_solid(color_leds, NUM_LEDS, CRGB::Black); // Éteindre toutes les LEDs
+		FastLED.show();
+	}
+}
+
+
+/*
+    --------- --------- --------- --------- ---------- ---------- ----------
+  --------- --------- --------- ------------- ---------- ---------- ----------
+--------- --------- --------- ----------------- ---------- ---------- ----------
+  --------- --------- --------- ------------- ---------- ---------- ----------
+    --------- --------- --------- --------- ---------- ---------- ----------
+*/
+
+/**
+*******************************
+* Led_Intensity
+* Cette fonction ajuste l'intensité lumineuse des LED.
+********************************
+*/
+void Led_Intensity(uint8_t newBrightness)
+{
+	brightness = newBrightness;
+	FastLED.setBrightness(brightness);
+}
+
+
+/**
+********************************
+* --- Start_Effect
+*******************************
+*/
+// Fonction pour l'effet de chargement
+void Start_Effect()
+{
+	#define START_DELAY 50 // Délai entre chaque étape de chargement en millisecondes
+
+	// Réglage des paramètres de l'effet de démarrage
+	Led_Intensity(200);
+	int numIterations = 3;
+	int delayPerIteration = 1000;
+	CRGB colors[] = {CRGB::Red, CRGB::Orange, CRGB::Green};
+	int numColors = sizeof(colors) / sizeof(colors[0]);
+	bool sound_played = false;
+
+
+	for (int i = 0; i < numIterations && loading_active; i++)
+	{ // Boucle pour chaque tour et arrêter si le chargement n'est plus actif
+		sound_played = false; // Indique si le son a été joué pour cette itération
+		for (int j = 0; j < NUM_LEDS; j++)
+		{ // Boucle pour chaque LED
+			fill_solid(color_leds, NUM_LEDS, CRGB::Black); // Éteindre toutes les LEDs
+			for (int k = 0; k < 3; k++)
+			{ // Boucle pour chaque groupe de 3 LEDs
+				color_leds[(j + k) % NUM_LEDS] = colors[i % numColors]; // Changer la couleur des trois LEDs
+			}
+			FastLED.show(); // Mettre à jour l'affichage des LEDs
+			if (buzzer_ON && !sound_played)
+			{ // Vérifie si le son n'a pas été joué pour cette itération
+				tone(tonePin, 1047, 150); // Do5 (C5), durée de la note : 150 ms
+				delay(150);
+				noTone(tonePin); // Éteindre le buzzer
+				sound_played = true; // Marque le son comme joué
+			}
+			delay(100); // Attendre un court instant entre les bips
+		}
+	}
+
+	if (loading_active) // Si le chargement est encore actif
+	{
+		// Allumer toutes les LEDs en bleu
+		fill_solid(color_leds, NUM_LEDS, CRGB::Blue);
+		FastLED.show();
+
+		// Vérifier si le buzzer est activé
+		if (buzzer_ON)
+		{
+			// Jouer le son spécial
+			tone(tonePin, 1294.54, 1000); // Do5 (C5), durée de la note : 800 ms
+			delay(1000); // Attendre la durée du son spécial
+			noTone(tonePin); // Arrêter le son spécial
+		}
+		else
+		{
+			// Attendre 800 ms sans jouer de son
+			delay(1000);
+		}
+
+		// Éteindre toutes les LEDs
+		fill_solid(color_leds, NUM_LEDS, CRGB::Black);
+		FastLED.show();
+		loading_active = false; // Arrêter le chargement
+
+		if (!reflexTimeRunning)
+		{
+			reflexTimeRunning = true;
+			Reflex_Time();
+		}
+	}
+}
+
+
+/**
+********************************
+* --- Commentaires
+*******************************
+*/
+void sound()
+{
+	bool ETAT_SOUND = digitalRead(SOUND_PIN);
+
+	if (ETAT_SOUND)
+	{
+		buzzer_ON = true;
+		Serial.println("Le son est détecté.");
+	}
+	else
+	{
+		buzzer_ON = false;
+		Serial.println("Le son n'est pas detecté.");
+	}
+}
+
+
+/**
+********************************
+* --- Commentaires
+*******************************
+*/
+// Mode d'entraînement - Réfléx-Time
+void Reflex_Time()
+{
+	// Vérifie si le jeu n'est pas déjà en cours
+	if (!reflexTimeRunning)
+	{
+		reflexTimeRunning = true;
+		switch_colorLed = false;
+
+		// Appel les fonctions
+		Start_Effect();
+		delay(1000);
+		Led_Intensity(20);
+
+		// Temps de départ du jeu | Temps écouler depuis le début du jeu
+		unsigned long startTime = millis();
+		unsigned long elapsedTime = 0;
+
+		// Jouer le jeu pendant x temps : 1000 = 1 secondes
+		while (elapsedTime < 100000)
+		{
+			// Sélectionner aléatoirement la couleur à afficher | Vert et Rouge
+			CRGB color = random(2) ? CRGB::Green : CRGB::Red;
+
+			// Afficher la couleur pendant 1,5 secondes
+			fill_solid(color_leds, NUM_LEDS, color);
+			FastLED.show();
+			delay(1500);
+
+			// Éteindre les LED's pendant une durée aléatoire entre 0.5 et 3 secondes
+			unsigned long offTime = random(500, 3001);
+			fill_solid(color_leds, NUM_LEDS, CRGB::Black);
+			FastLED.show();
+			delay(offTime);
+
+			// Mettre à jou temps écoulé depuis le début du jeu
+			elapsedTime = millis() - startTime;
+		}
+
+		// Effet de fin de jeu
+		endGameEffect();
+
+		// Mettre à jour l'état du jeu pour indiquer qu'il est en cours
+		reflexTimeRunning = false;
+	}
+}
+
+
+/**
+********************************
+* --- Commentaires
+*******************************
+*/
+// Fonction Fin du Jeu
+void endGameEffect()
+{
+	// Définir la vitesse de rotation des couleurs et la luminosité
+	uint8_t colorRotationSpeed = 2;
+	Led_Intensity(200);
+
+	// Boucle infinie pour afficher l'effet de fin de jeu
+	while (true)
+	{
+		// Faire tourner les couleurs et jouer le son
+		for (int i = 0; i < NUM_LEDS; i++)
+		{
+			// Jouer un son pour indiquer la fin du jeu
+			tone(tonePin, 1319, 50); // Note Do6 pendant 250 ms
+			delay(20);
+			tone(tonePin, 1175, 50); // Note La5 pendant 250 ms
+			delay(20);
+			tone(tonePin, 1047, 50); // Note Do5 pendant 250 ms
+			delay(20);
+
+			// Allumer les LED avec la couleur actuelle | Les afficher
+			color_leds[i] = ColorFromPalette(RainbowColors_p, millis() / colorRotationSpeed + i * 100);
+			FastLED.show();
+
+			// Attendre un court instant avant de passer à la LED suivante
+			delay(10);
+		}
+
+		// Faire clignoter les LEDs doucement
+		for (int j = 0; j < 256; j++)
+		{
+			for (int i = 0; i < NUM_LEDS; i++)
+			{
+				// Faire disparaître la luminosité lentement
+				color_leds[i].fadeToBlackBy(1);
+			}
+
+			// Afficher les LEDs | et attendre entre chaque clignotement
+			FastLED.show();
+			delay(10);
+		}
+	}
+}
+
+
+/**
+********************************
+* --- Commentaires
+*******************************
+*/
+// Fonction pour changer de couleur en fonction de la télécommande IR
+void Switch_Color()
+{
+	const unsigned long PROGMEM expectedHex_switch_color_rouge = 0xFF6897;
+	const unsigned long PROGMEM expectedHex_switch_color_vert = 0xFF7A85;
+	const unsigned long PROGMEM expectedHex_switch_color_bleu = 0xFF52AD;
+	const unsigned long PROGMEM expectedHex_switch_color_blanc = 0xFF42BD;
+
+	if (irrecv.decode(&results))
+	{
+		Serial.print("Code IR reçu: 0x");
+		Serial.println(results.value, HEX); // Affiche le code reçu en HEX.
+
+		if (results.value == expectedHex_switch_color_rouge)
+		{
+			// Change toutes les LEDs en rouge
+			fill_solid(color_leds, NUM_LEDS, CRGB(255, 0, 0));
+			Serial.println("Switch color Rouge");
+			switch_colorLed = true;
+			FastLED.show();
+		}
+		if (results.value == expectedHex_switch_color_vert)
+		{
+			// Change toutes les LEDs en vert
+			fill_solid(color_leds, NUM_LEDS, CRGB(0, 255, 0));
+			Serial.println("Switch color Vert");
+			switch_colorLed = true;
+			FastLED.show();
+		}
+		if (results.value == expectedHex_switch_color_bleu)
+		{
+			// Change toutes les LEDs en bleu
+			fill_solid(color_leds, NUM_LEDS, CRGB(0, 0, 255));
+			Serial.println("Switch color Bleu");
+			switch_colorLed = true;
+			FastLED.show();
+		}
+		if (results.value == expectedHex_switch_color_blanc)
+		{
+			// Change toutes les LEDs en blanc
+			fill_solid(color_leds, NUM_LEDS, CRGB(255, 255, 255));
+			Serial.println("Switch color Blanc");
+			switch_colorLed = true;
+			FastLED.show();
+		}
+		// D'autre condition ici (S'il en a d'autre à l'avenir)
+
+		// Prépare pour le prochain signal IR
+		irrecv.resume();
+	}
+}
