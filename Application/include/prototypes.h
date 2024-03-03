9713304d10de32f22b543da6d0942133c011e492
Air-KS <kevinrogeret@gmail.com>
2024-03-04 00:17:31 +0100

    [Create] - New file prototype.h : Add new functions prototype

---
+++ Application/include/prototypes.h
@@ -0,0 +1,17 @@
+// prototype.h
+#include "main.h"
+
+#ifndef PROTOTYPE_H
+#define PROTOTYPE_H
+
+// Prototypes de fonctions
+void Led_Intensity(uint8_t newBrightness);
+void Start_Effect();
+void setup();
+void loop();
+void sound();
+void Reflex_Time();
+void Switch_Color();
+void endGameEffect();
+
+#endif
