932d401cd04fe5ca82a5b012325274b4bd96f152
Air-KS <kevinrogeret@gmail.com>
2024-03-04 00:16:28 +0100

    [Create] - New file broche.h : Add define for broche

---
+++ Application/include/broches.h
@@ -0,0 +1,18 @@
+// Broches.h
+#include "main.h"
+#include "prototypes.h"
+
+#ifndef BROCHE_H
+#define BROCHE_H
+
+// Définition des constantes pour les broches
+#define DATA_PIN 33
+#define NUM_LEDS 10
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+#define REFLEX_TIME 22
+#define SWITCH_COLOR 22
+#define SOUND_PIN 4
+#define tonePin 12
+
+#endif
