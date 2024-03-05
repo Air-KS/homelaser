0b7d72cdec5ed5fa6ddff6685965420f9439a431
Air-KS <kevinrogeret@gmail.com>
2024-03-05 02:37:27 +0100

    [Create] - File pitches.h | Definition of music notes frequencies

---
+++ Application/include/pitches.h
@@ -0,0 +1,177 @@
+#ifndef PITCHES_H
+#define PITCHES_H
+
+/**
+***********************************************************
+* Définition des fréquences des notes de musique
+* Comprendre les variable:
+* --- NOTES_1DO = La note do de la première octave ---
+* --- NOTE_8DO = La note do de la 8ème octave ---
+* Les lettres B et D représente les dièse et les bémole
+***********************************************************
+*/
+
+/**
+***********************************************************
+* Par exemple pour Au clair de la lune
+* en 4eme Octave
+* NOTE_4DO 262 | NOTE_4DO 262 | NOTE_4DO 262
+* NOTE_4RE 294 | NOTE_4MI 330 | NOTE_4RE 294
+* NOTE_4DO 262 | NOTE_4MI 330 | NOTE_4RE 294
+* NOTE_4RE 294 | NOTE_4DO 262
+***********************************************************
+*/
+
+// Notes de la 1ère octave
+#define NOTE_1DO 32
+#define NOTE_1DO_D 33
+#define NOTE_1RE 36
+#define NOTE_1RE_D 37
+#define NOTE_1MI 40
+#define NOTE_1FA 43
+#define NOTE_1FA_D 45
+#define NOTE_1SOL 48
+#define NOTE_1SOL_D 50
+#define NOTE_1LA 53
+#define NOTE_1LA_D 55
+#define NOTE_1SI 58
+#define NOTE_1DO_B 30
+#define NOTE_1RE_B 35
+#define NOTE_1FA_B 41
+#define NOTE_1SOL_B 46
+#define NOTE_1LA_B 51
+
+// Notes de la 2ème octave
+#define NOTE_2DO 65
+#define NOTE_2DO_D 69
+#define NOTE_2RE 73
+#define NOTE_2RE_D 78
+#define NOTE_2MI 82
+#define NOTE_2FA 87
+#define NOTE_2FA_D 92
+#define NOTE_2SOL 98
+#define NOTE_2SOL_D 104
+#define NOTE_2LA 110
+#define NOTE_2LA_D 117
+#define NOTE_2SI 123
+#define NOTE_2DO_B 61
+#define NOTE_2RE_B 69
+#define NOTE_2FA_B 82
+#define NOTE_2SOL_B 92
+#define NOTE_2LA_B 103
+
+// Notes de la 3ème octave
+#define NOTE_3DO 131
+#define NOTE_3DO_D 139
+#define NOTE_3RE 147
+#define NOTE_3RE_D 156
+#define NOTE_3MI 165
+#define NOTE_3FA 175
+#define NOTE_3FA_D 185
+#define NOTE_3SOL 196
+#define NOTE_3SOL_D 208
+#define NOTE_3LA 220
+#define NOTE_3LA_D 233
+#define NOTE_3SI 247
+#define NOTE_3DO_B 123
+#define NOTE_3RE_B 138
+#define NOTE_3FA_B 165
+#define NOTE_3SOL_B 185
+#define NOTE_3LA_B 207
+
+// Notes de la 4ème octave
+#define NOTE_4DO 262
+#define NOTE_4DO_D 277
+#define NOTE_4RE 294
+#define NOTE_4RE_D 311
+#define NOTE_4MI 330
+#define NOTE_4FA 349
+#define NOTE_4FA_D 370
+#define NOTE_4SOL 392
+#define NOTE_4SOL_D 415
+#define NOTE_4LA 440
+#define NOTE_4LA_D 466
+#define NOTE_4SI 494
+#define NOTE_4DO_B 247
+#define NOTE_4RE_B 277
+#define NOTE_4FA_B 330
+#define NOTE_4SOL_B 370
+#define NOTE_4LA_B 415
+
+// Notes de la 5ème octave
+#define NOTE_5DO 523
+#define NOTE_5DO_D 554
+#define NOTE_5RE 587
+#define NOTE_5RE_D 622
+#define NOTE_5MI 659
+#define NOTE_5FA 698
+#define NOTE_5FA_D 740
+#define NOTE_5SOL 784
+#define NOTE_5SOL_D 831
+#define NOTE_5LA 880
+#define NOTE_5LA_D 932
+#define NOTE_5SI 988
+#define NOTE_5DO_B 494
+#define NOTE_5RE_B 554
+#define NOTE_5FA_B 659
+#define NOTE_5SOL_B 740
+#define NOTE_5LA_B 831
+
+// Notes de la 6ème octave
+#define NOTE_6DO 1047
+#define NOTE_6DO_D 1109
+#define NOTE_6RE 1175
+#define NOTE_6RE_D 1245
+#define NOTE_6MI 1319
+#define NOTE_6FA 1397
+#define NOTE_6FA_D 1480
+#define NOTE_6SOL 1568
+#define NOTE_6SOL_D 1661
+#define NOTE_6LA 1760
+#define NOTE_6LA_D 1865
+#define NOTE_6SI 1976
+#define NOTE_6DO_B 988
+#define NOTE_6RE_B 1109
+#define NOTE_6FA_B 1319
+#define NOTE_6SOL_B 1480
+#define NOTE_6LA_B 1661
+
+// Notes de la 7ème octave
+#define NOTE_7DO 2093
+#define NOTE_7DO_D 2217
+#define NOTE_7RE 2349
+#define NOTE_7RE_D 2489
+#define NOTE_7MI 2637
+#define NOTE_7FA 2794
+#define NOTE_7FA_D 2960
+#define NOTE_7SOL 3136
+#define NOTE_7SOL_D 3322
+#define NOTE_7LA 3520
+#define NOTE_7LA_D 3729
+#define NOTE_7SI 3951
+#define NOTE_7DO_B 1976
+#define NOTE_7RE_B 2217
+#define NOTE_7FA_B 2637
+#define NOTE_7SOL_B 2960
+#define NOTE_7LA_B 3322
+
+// Notes de la 8ème octave
+#define NOTE_8DO 4186
+#define NOTE_8DO_D 4435
+#define NOTE_8RE 4699
+#define NOTE_8RE_D 4978
+#define NOTE_8MI 5274
+#define NOTE_8FA 5588
+#define NOTE_8FA_D 5920
+#define NOTE_8SOL 6272
+#define NOTE_8SOL_D 6645
+#define NOTE_8LA 7040
+#define NOTE_8LA_D 7459
+#define NOTE_8SI 7902
+#define NOTE_8DO_B 3951
+#define NOTE_8RE_B 4435
+#define NOTE_8FA_B 5274
+#define NOTE_8SOL_B 5920
+#define NOTE_8LA_B 6645
+
+#endif
