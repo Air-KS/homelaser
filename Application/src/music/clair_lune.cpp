65e19c9cbcd045ac3b2a2af2e9889b3c256953ed
Air-KS <kevinrogeret@gmail.com>
2024-03-07 00:59:33 +0100

    [Create Music] - By the light of the moon

---
+++ Application/src/music/clair_lune.cpp
@@ -0,0 +1,35 @@
+#include "pitches.h"
+#include "broches.h"
+
+/**
+********************************
+* ----- Musique -----
+* Au clair de la Lune
+*******************************
+*/
+void Music_Clair_Lune()
+{
+	tone(tonePin, NOTE_4DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_4DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_4DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_4RE, 500);
+	delay(520);
+	tone(tonePin, NOTE_4MI, 1000);
+	delay(1020);
+	tone(tonePin, NOTE_4RE, 500);
+	delay(1000);
+	tone(tonePin, NOTE_4DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_4MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_4RE, 500);
+	delay(520);
+	tone(tonePin, NOTE_4RE, 500);
+	delay(520);
+	tone(tonePin, NOTE_4DO, 1000);
+	delay(5000);
+	return;
+}
