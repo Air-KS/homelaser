4d890006e7f906cb296a7be4dbf0384018c091ac
Air-KS <kevinrogeret@gmail.com>
2024-03-07 01:00:16 +0100

    [Create music] - Little Start

---
+++ Application/src/music/little_star.cpp
@@ -0,0 +1,104 @@
+#include "pitches.h"
+#include "broches.h"
+
+/**
+********************************
+* ----- Musique -----
+* Twinkle Twinkle Little Start
+*******************************
+*/
+
+void Little_Start()
+{
+	tone(tonePin, NOTE_3DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_3DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3LA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3LA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 1000);
+	delay(1100);
+
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_3MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_3RE, 500);
+	delay(520);
+	tone(tonePin, NOTE_3RE, 500);
+	delay(520);
+	tone(tonePin, NOTE_3DO, 1000);
+	delay(1100);
+
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_3MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_3RE, 500);
+	delay(700);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_3MI, 500);
+	delay(520);
+	tone(tonePin, NOTE_3RE, 1000);
+	delay(1100);
+
+	tone(tonePin, NOTE_3DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_3DO, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 500);
+	delay(520);
+	tone(tonePin, NOTE_3LA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3LA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3SOL, 1000);
+	delay(1100);
+
+	tone(tonePin, NOTE_3FA, 500);
+	delay(520);
+	tone(tonePin, NOTE_3FA, 550);
+	delay(570);
+	tone(tonePin, NOTE_3MI, 600);
+	delay(620);
+	tone(tonePin, NOTE_3MI, 650);
+	delay(670);
+	tone(tonePin, NOTE_3RE, 700);
+	delay(720);
+	tone(tonePin, NOTE_3RE, 750);
+	delay(770);
+	tone(tonePin, NOTE_3DO, 1100);
+	delay(1100);
+
+	delay(5000);
+	return;
+}
