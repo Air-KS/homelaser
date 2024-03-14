0471aa6130b53a8cab28be48270d83ca8f772fa5
Air-KS <kevinrogeret@gmail.com>
2024-03-14 05:28:22 +0100

    [Update] - Add variable global

--- Application/src/main.h
+++ Application/src/main.h
@@ -3,10 +3,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+//
 #include <Arduino.h>
 #include <FastLED.h>
 #include <IRrecv.h>
 #include <IRutils.h>
 #include "wifi.h"
+
+//
+#include "broches.h"
+
+//
+extern CRGB color_leds[NUM_LEDS];
+extern decode_results results;
+extern const uint16_t kRecvPin;
+extern IRrecv irrecv;
+
 
 #endif
