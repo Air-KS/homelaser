92011eb60a81581b98da1f6a2bf7845247975395
Air-KS <kevinrogeret@gmail.com>
2024-05-21 22:01:59 +0200

    New file for test color in react web site

---
+++ Application/src/games/color.cpp
@@ -0,0 +1,17 @@
+/*****
+	./src/games/color.cpp
+ *****/
+
+#include "games.h"
+
+void changeColor(String color) {
+	if (color == "red"){
+		// Code to turn LEDs red
+	} else if (color == "green"){
+		// Code to turn LEDs green
+	} else if (color == "blue"){
+		// Code to turn LEDs blue
+	} else if (color == "yellow"){
+		// Code to turn LEDs yellow
+	}
+}
